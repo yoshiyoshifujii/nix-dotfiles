c3ec1a6f4dd76e46492dd1ced0597c99d5375e79
FUJII Yoshitaka <yoshiyoshifujii@gmail.com>
2026-02-01 14:42:59 +0900

    wezterm キーバインドをtmuxから引き継いだ

--- home/default.nix
+++ home/default.nix
@@ -7,23 +7,9 @@
       home.username = username;
       home.stateVersion = "24.05";
 
-      home.packages = [
-        pkgs.meslo-lgs-nf
+      imports = [
+        ./wezterm.nix
       ];
-
-      xdg.configFile."wezterm/wezterm.lua".text = ''
-        local wezterm = require("wezterm")
-        return {
-          font_dirs = { "${pkgs.meslo-lgs-nf}/share/fonts/truetype" },
-          font = wezterm.font("MesloLGS NF", { weight = "Regular" }),
-          font_size = 13.0,
-          color_scheme = "3024 Night",
-          font_rasterizer = "FreeType",
-          freetype_load_target = "Normal",
-          freetype_render_target = "Normal",
-          window_background_opacity = 0.8,
-        }
-      '';
     };
   };
 }
