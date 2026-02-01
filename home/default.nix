56e77d9289357a088002f996de1bcded3de5cf9c
FUJII Yoshitaka <yoshiyoshifujii@gmail.com>
2026-02-01 13:49:06 +0900

    font

--- home/default.nix
+++ home/default.nix
@@ -14,8 +14,13 @@
       xdg.configFile."wezterm/wezterm.lua".text = ''
         local wezterm = require("wezterm")
         return {
+          font_dirs = { "${pkgs.meslo-lgs-nf}/share/fonts/truetype" },
           font = wezterm.font("MesloLGS NF", { weight = "Regular" }),
           font_size = 13.0,
+          color_scheme = "3024 Night",
+          font_rasterizer = "FreeType",
+          freetype_load_target = "Normal",
+          freetype_render_target = "Normal",
           window_background_opacity = 0.8,
         }
       '';
