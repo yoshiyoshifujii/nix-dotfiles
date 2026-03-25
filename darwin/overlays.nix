0ec4b824b80b7ffdd4806b5ba62096fd4c081462
FUJII Yoshitaka <yoshiyoshifujii@gmail.com>
2026-03-25 18:01:58 +0900

    Add TODO for direnv darwin overlay removal

--- darwin/overlays.nix
+++ darwin/overlays.nix
@@ -4,6 +4,7 @@
     (_final: prev: {
       # direnv 2.37.1 from current nixpkgs fails on aarch64-darwin because
       # it requests external linking while CGO is disabled by default.
+      # TODO: Remove this overlay once nixpkgs fixes direnv on aarch64-darwin.
       direnv = prev.direnv.overrideAttrs (old: {
         env = (old.env or { }) // {
           CGO_ENABLED = "1";
